569207273f1b39a86d779fbd3e414661f9d5fec7
Pranjul <prpranjul@gmail.com>
2023-09-19 13:27:04 +0530

    added environment setup script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 apps_to_deploy="./configs/applications.conf"
 while IFS= read -r line
 do
-  eval "sudo docker compose -f src/docker-compose/$line/docker-compose.yml --env-file configs/conf.env config"
+  eval "docker compose -f src/docker-compose/$line/docker-compose.yml --env-file configs/conf.env config"
 done < "$apps_to_deploy"
---
+++ env_setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "checking git installation"
+if ! command -v git > /dev/null
+then
+    echo "git not installed"
+    echo "installing git"
+
+    sudo apt-get update
+    sudo apt install git
+else
+    echo "git already installed"
+    echo "skipping git installation"
+fi
+
+echo "checking docker installation"
+if ! command -v docker > /dev/null
+then
+    echo "docker not installed"
+    echo "installing docker"
+
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Add the repository to Apt sources:
+    echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+    echo "docker already installed"
+    echo "skipping docker installation"
+fi
+
+echo "env setup done"
+echo "edit config files and run deploy.sh file"
